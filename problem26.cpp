bdeeb6635b9ae3428baceb95006f7c5db8e88335
seasami <2256160101@hcmussh.edu.vn>
2026-06-26 19:50:24 +0700

    Commit for Problem 26

---
+++ problem26.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    // Nếu mảng rỗng thì dừng
+    if (nums.empty()) return 0;
+    // Trường hợp còn lại
+    int count = 0;
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] != nums[count]) {
+            count++;
+            nums[count] = nums[i]; 
+        }
+    }
+    return count + 1;
+}
+
+int main() {
+    int k; cin >> k;
+    vector<int> n(k);
+    for (int i = 0; i < k; i++) cin >> n[i];
+
+    cout << removeDuplicates(n);
+}
