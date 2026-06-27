af0b490645d8561e9d71b9b6e55c735f99722fda
seasami <2256160101@hcmussh.edu.vn>
2026-06-27 16:06:46 +0700

    Update problem26.cpp

--- problem26.cpp
+++ problem26.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
     // Nếu mảng rỗng thì dừng
-    if (nums.empty()) return 0;
+    if (nums.empty()) return -1;
     // Trường hợp còn lại
     int count = 0;
     for (int i = 1; i < nums.size(); i++) {
