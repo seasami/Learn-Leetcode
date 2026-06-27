34af31c746920f93136da49e47b8587bc8de066f
seasami <2256160101@hcmussh.edu.vn>
2026-06-27 16:51:43 +0700

    Commit for Problem 3 in cpp

---
+++ problem3.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    // Nếu chuỗi rỗng thì thoát
+    if (s.empty()) return -1;
+    // Tạo mảng để lưu trữ vị trí trong chuỗi của một ký tự bất kỳ
+    int last_seen[128];
+    for (int i = 0; i < 128; i ++) last_seen[i] = -1;
+
+    int left = 0, max_length = 0;
+    for (int right = 0; s[right] != '\0'; right++) {
+        char curr = s[right];
+        if (last_seen[curr] >= left) {
+            left = last_seen[curr] + 1;
+        }
+        last_seen[curr] = right;
+        max_length = max(max_length, right - left + 1);
+    }
+    return max_length;
+}
+
+int main() {
+    string s; getline(cin, s);
+    cout << lengthOfLongestSubstring(s);
+}
